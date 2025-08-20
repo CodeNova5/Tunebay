87f6a4ae29b8664d07e37823a155141abd418391
Code Nova <netdot1234@gmail.com>
2025-08-20 22:11:42 +0100

    rjj

--- api/comments/uploadFile.js
+++ api/comments/uploadFile.js
@@ -43,9 +43,8 @@ export default async function handler(req, res) {
 
         const owner = "CodeNova5";
         const repo = "Music-Backend";
-        const safeArtistName = encodeURIComponent(artistName);
-        const safeFileName = encodeURIComponent(uploadedFileName);
-        const path = `public/music/${safeArtistName}/${safeFileName}`;
+        const path = `public/music/${artistName}/${uploadedFileName}`;
+
         let sha;
         try {
           const { data } = await octokit.rest.repos.getContent({ owner, repo, path });
