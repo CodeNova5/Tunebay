ef09c5127819729f6c602918482acb290df5fbfa
Code Nova <160973282+CodeNova5@users.noreply.github.com>
2025-09-19 16:26:39 +0100

    Update ArtistPage.tsx

--- app/music/[artist]/ArtistPage.tsx
+++ app/music/[artist]/ArtistPage.tsx
@@ -118,7 +118,7 @@ export default function ArtistPage() {
             letterSpacing: "1px",
           }}
         >
-          Followers: {artistDetails.followers}
+          Followers: {artistDetails.followers?.toLocaleString()}
         </span>
       </div>
       <CommentShareModule playlist={undefined} track={undefined} album={undefined} artist={artistDetails} />
