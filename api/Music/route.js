2171b40b1029cf417ff91a2a4e817c245ce90f73
Code Nova <netdot1234@gmail.com>
2025-10-05 00:24:22 +0100

    ...

--- api/Music/route.js
+++ api/Music/route.js
@@ -389,7 +389,6 @@ export default async function handler(req, res) {
       }
     }
 
-
     else if (type === "lyricsVideo") {
       if (!songName || !artistName) {
         return res.status(400).json({ error: "Missing song name or artist name" });
