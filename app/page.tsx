dbc00cbab40ebf84a3ac61c423c2da08aa90724a
Code Nova <netdot1234@gmail.com>
2025-10-17 23:40:03 +0100

    og-image

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
       {
         url: 'https://tunebay.vercel.app/images/og-image.jpg',
         width: 1200,
-        height: 630,
+        height: 630, 
 
         alt: 'Tunebay logo',
       },
