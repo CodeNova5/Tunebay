958f5a51a7802862e1984482472e4fcc1d23e679
Code Nova <160973282+CodeNova5@users.noreply.github.com>
2025-10-18 10:06:19 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,16 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import NotificationModal from "@/components/NotificationModal";
+// app/layout.tsx
+import ErudaClient from './ErudaClient';
 
+export const metadata = {
+  title: 'Tunebay',
+  description: 'Stream and discover music',
+};
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>)
-{
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#0049af" />
-        <link rel="apple-touch-icon" href="/favicon.ico" />
-  
-      </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <NotificationModal />
+      <body>
+        <ErudaClient />
         {children}
       </body>
     </html>
