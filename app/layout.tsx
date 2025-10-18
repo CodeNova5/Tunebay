5ae8aa5e2e3e4a07d6cc227e7d081c99e632f2fe
Code Nova <netdot1234@gmail.com>
2025-10-18 10:38:39 +0100

    fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,18 @@
 // app/layout.tsx
 import ErudaClient from './ErudaClient';
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: 'Tunebay',
@@ -9,7 +22,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ErudaClient />
         {children}
       </body>
